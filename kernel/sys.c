01be6ec703ff5aa13cfeb9205b84962716918ba6
NKR <14344392+NKR00711@users.noreply.github.com>
2025-12-21 12:01:47 +0530

    Comment out pr_debug for uname release
    Comment out debug log for fake uname.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1266,8 +1266,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netd", 4) ||
 	    !strncmp(current->comm, "uprobestats", 11)) {
 		strcpy(tmp.release, "5.10.240");
-		pr_debug("fake uname: %s release=%s\n",
-			 current->comm, tmp.release);
+		// pr_debug("fake uname: %s release=%s\n",
+			 // current->comm, tmp.release);
 	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
