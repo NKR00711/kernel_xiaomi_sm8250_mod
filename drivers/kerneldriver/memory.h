16b4649f1ebf31806d5ca5370eac37b1d87e61ad
NKR <14344392+NKR00711@users.noreply.github.com>
2026-01-05 21:06:54 +0530

    Update memory.h

--- drivers/kerneldriver/memory.h
+++ drivers/kerneldriver/memory.h
@@ -1,12 +1,10 @@
+#ifndef _MEMORY_H_
+#define _MEMORY_H_
+
 #include <linux/kernel.h>
-#include <linux/sched.h>
-
-phys_addr_t translate_linear_address(struct mm_struct *mm, uintptr_t va);
-
-bool read_physical_address(phys_addr_t pa, void *buffer, size_t size);
-
-bool write_physical_address(phys_addr_t pa, void *buffer, size_t size);
+#include <linux/types.h>
 
 bool read_process_memory(pid_t pid, uintptr_t addr, void *buffer, size_t size);
-
 bool write_process_memory(pid_t pid, uintptr_t addr, void *buffer, size_t size);
+
+#endif /* _MEMORY_H_ */
