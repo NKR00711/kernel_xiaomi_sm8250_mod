fe74158cbcc879253dd69f4fdeca8e998d478171
NKR <14344392+NKR00711@users.noreply.github.com>
2026-02-04 12:58:19 +0530

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1266,12 +1266,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4) ||
 	    !strncmp(current->comm, "uprobestats", 11)) {
-		// if(current_uid().val == 0) {
-		// 	strcpy(tmp.release, "5.10.240");
-		// }
-		// else {
+		if(unlikely(current_uid().val == 0)) {
+			strcpy(tmp.release, "5.10.240");
+		}
+		else {
 			strcpy(tmp.release, "4.19.325-perf+");
-		// }
+		}
 		pr_info("fake uname: %s release=%s\n",
 			 current->comm, tmp.release);
 	}
