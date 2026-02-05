e076f4eaff3068b2b7ebc73aed2931882f1324ec
NKR <14344392+NKR00711@users.noreply.github.com>
2026-02-05 12:42:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -95,8 +95,8 @@ echo "TARGET_DEVICE: $TARGET_DEVICE"
 
 if [ $KSU_ENABLE -eq 1 ]; then
     echo "KSU is enabled"
-    curl -LSs "https://raw.githubusercontent.com/NKR00711/ReSukiSU/main/kernel/setup.sh" | bash -s ApartTUSITU
-    # curl -LSs "https://raw.githubusercontent.com/ApartTUSITU/ReSukiSU/main/kernel/setup.sh" | bash -s ApartTUSITU
+    # curl -LSs "https://raw.githubusercontent.com/NKR00711/ReSukiSU/main/kernel/setup.sh" | bash -s ApartTUSITU
+    curl -LSs "https://raw.githubusercontent.com/ApartTUSITU/ReSukiSU/main/kernel/setup.sh" | bash -s ApartTUSITU
     # curl -LSs "https://raw.githubusercontent.com/NKR00711/SukiSU-Ultra-4.19/main/kernel/setup.sh" | bash -s NKR00711
 else
     echo "KSU is disabled"
@@ -273,6 +273,7 @@ if [ $KSU_ENABLE -eq 1 ]; then
     -e KSU_SUSFS_SUS_MAP \
     -e THREAD_INFO_IN_TASK \
     -e KPM
+    -e KSU_MULTI_MANAGER_SUPPORT
 else
     scripts/config --file out/.config -d KSU
 fi
